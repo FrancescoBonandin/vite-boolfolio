0994fc50418d4bf3bc4fd28ad4d6d5344f84a529
FrancescoBonandin <bonandinfrancesco@gmail.com>
2023-09-27 18:05:17 +0200

    EDIT milestone 5 completata

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,8 +1,12 @@
 <script>
 import axios from 'axios';
+import ProjectCardComponent from "./ProjectCardComponent.vue";
 
 export default {
     name: "MainComponent",
+    components:{
+        ProjectCardComponent
+    },
     data() {
         return {
             projects:[],
@@ -29,62 +33,9 @@ export default {
     <main>
         <div class="container">
             <div class="row justify-content-between ">
-                <div v-for="project in projects" class="col-4 g-4">
+                <div v-for="project in projects" class="col-3 g-4">
                  
-                    <div  class="card h-100 bg-secondary text-light">
-                        <h2 class="card-title ">
-                            {{ project.title }}
-                        </h2>
-                        <div class="card-body bg-dark">
-                            <img v-if="project.img != null" :src="'C:/MAMP/htdocs/Boolean/laravel-many-to-many/public/storage/'+project.img" :alt="project.title" class="card-img img-fluid">
-
-                            <div>
-                                <h4>
-                                    Description:
-                                </h4>
-                                {{ project.description }}
-                            </div>
-
-                            <div >
-
-                                <h5>
-                                    Type:
-                                </h5>
-
-
-                                <div v-if="project.type !=null">
-                                    {{ project.type.type_name}}
-                                </div>
-
-                                <div v-else>
-                                    -
-                                </div>
-
-                            </div>
-
-                            <div>
-
-                                <h5>
-                                    Technologies:
-                                </h5>
-
-                                <div v-if="project.technologies.length>0">
-                                    <span v-for="(technology,key) in project.technologies" :key="key">
-                                        {{technology.technology_name}}
-                                        <span v-if="key<project.technologies.length-1">
-                                          {{', '}}
-                                        </span>
-                                    </span>
-                                   
-                                </div>
-    
-                                <div v-else>
-                                    -
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
+                    <ProjectCardComponent   :singleProject="project" />
                 </div>
             </div>
         </div>
@@ -93,10 +44,6 @@ export default {
 
 <style lang="scss" scoped>
 
-    .card-title{
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-    }
+ 
 
 </style>
---
+++ src/components/ProjectCardComponent.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+    name: "ProjectCardComponent",
+    data() {
+        return {
+
+        }
+    },
+    props:{
+        singleProject:Object,
+        
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<template>
+    <div  class="card h-100 bg-secondary text-light">
+        <h2 class="card-title ">
+            {{ singleProject.title }}
+        </h2>
+        <div class="card-body bg-dark">
+            <img v-if="singleProject.img != null" :src="'C:/MAMP/htdocs/Boolean/laravel-many-to-many/public/storage/'+singleProject.img" :alt="singleProject.title" class="card-img img-fluid">
+
+            <div>
+                <h4>
+                    Description:
+                </h4>
+                {{ singleProject.description }}
+            </div>
+
+            <div >
+
+                <h5>
+                    Type:
+                </h5>
+
+
+                <div v-if="singleProject.type !=null">
+                    {{ singleProject.type.type_name}}
+                </div>
+
+                <div v-else>
+                    -
+                </div>
+
+            </div>
+
+            <div>
+
+                <h5>
+                    Technologies:
+                </h5>
+
+                <div v-if="singleProject.technologies.length>0">
+                    <span v-for="(technology,key) in singleProject.technologies" :key="key">
+                        {{technology.technology_name}}
+                        <span v-if="key<singleProject.technologies.length-1">
+                            {{', '}}
+                        </span>
+                    </span>
+                    
+                </div>
+
+                <div v-else>
+                    -
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+.card-title{
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+</style>
